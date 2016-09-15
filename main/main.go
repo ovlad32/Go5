a73f6e72eaa6a9c038c22baebfc588fd8afe2b89
Vladymyr Orlovskiy <vladymyr.orlovskiy@rokittechua.com>
2016-09-15 19:36:10 +0300

    i1

--- main/main.go
+++ main/main.go
@@ -12,6 +12,7 @@ var Db *sql.DB;
 
 func DbInit(){
 	var err error;
+	//pwd for sbs:123123
 	Db,err = sql.Open("postgres", "user=gouser password=11 dbname=fin host=52.29.37.253 port=5432 sslmode=disable")
 	if err != nil {
 		log.Fatal(err);
